970ae0048a61717973fd5ff6f0131ccec3664c9f
Zheyu Song <zheyusong1999@gmail.com>
2025-11-06 03:22:27 +0100

    Create TLEM2_Tibia_Left_picked_points.pp

---
+++ Material/after_matching/left/Bone/Source/TLEM2_Tibia_Left_picked_points.pp
@@ -0,0 +1,29 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime time="08:51:08" date="2024-08-23"/>
+  <User name="johnstef"/>
+  <DataFileName name="TLEM2_Tibia_Left.stl"/>
+  <templateName name="Femur_Right_19.pptpl"/>
+ </DocumentData>
+ <point x="0.0035218867" z="-0.019035801" name="0" active="1" y="0.022007233"/>
+ <point x="-0.013667929" z="-0.0054701534" name="1" active="1" y="0.0175316"/>
+ <point x="0.0041657956" z="0.021387605" name="2" active="1" y="0.00073488033"/>
+ <point x="0.023116333" z="0.015738975" name="3" active="1" y="-0.0018640932"/>
+ <point x="0.013836043" z="0.028527988" name="4" active="1" y="0.00012044359"/>
+ <point x="-0.01403035" z="0.00053507602" name="5" active="1" y="0.0062119924"/>
+ <point x="0.0081035132" z="0.0054574204" name="6" active="1" y="0.0090540629"/>
+ <point x="0.021524217" z="-0.011647451" name="7" active="1" y="0.14330514"/>
+ <point x="0.026291871" z="-0.0078785215" name="8" active="1" y="0.19825873"/>
+ <point x="0.032726366" z="-0.012270658" name="9" active="1" y="0.29527289"/>
+ <point x="-0.0041951346" z="-0.0074667833" name="10" active="1" y="0.33677161"/>
+ <point x="0.011958725" z="-0.012375253" name="11" active="1" y="0.33442262"/>
+ <point x="0.014760838" z="0.0089547196" name="12" active="1" y="0.33704594"/>
+ <point x="-0.002124167" z="0.0090374546" name="13" active="1" y="0.3372936"/>
+ <point x="0.0034303979" z="0.025520783" name="14" active="1" y="0.32801044"/>
+ <point x="0.00031026619" z="-0.022133648" name="15" active="1" y="0.33210084"/>
+ <point x="-0.020866489" z="-0.0090756696" name="16" active="1" y="0.32574669"/>
+ <point x="-0.023352396" z="0.0085604284" name="17" active="1" y="0.31590614"/>
+ <point x="-0.0046070851" z="-0.038385876" name="18" active="1" y="0.32350418"/>
+ <point x="-0.0034028275" z="0.038896222" name="19" active="1" y="0.32086378"/>
+</PickedPoints>
